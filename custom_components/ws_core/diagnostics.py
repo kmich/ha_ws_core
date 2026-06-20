4257fd5b4db400226e309b67a966a0bf7eca1dba
Konstantinos Michalitsis <kmichalitsis@gmail.com>
2026-06-20 09:25:47 +0300

    Fix diagnostics version mismatch

--- custom_components/ws_core/diagnostics.py
+++ custom_components/ws_core/diagnostics.py
@@ -56,7 +56,7 @@ async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigE
 
     return {
         "title": entry.title,
-        "version": "2.3.0",
+        "version": "2.3.1",
         "entry_data": _redact_coords(dict(entry.data)),
         "entry_options": _redact_coords(dict(entry.options)),
         "sources": sources,
